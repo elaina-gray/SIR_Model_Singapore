18afda24f650be9d9c608663c61c658438d2774c
Elaina Gray <182298b@student.hci.edu.sg>
2022-11-11 15:57:27 +0800

    changes

---
+++ SIR_Model_Singapore/main.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+fasf
+if __name__ == '__main__':
+
+    # Total population, N.
+    N = 5500000
+    # Initial number of infected and recovered individuals, I0 and R0.
+    I0, R0 = 47, 12
+    # Everyone else, S0, is susceptible to infection initially.
+    S0 = N - I0 - R0
+    # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+    beta, gamma= 0.157, 1./20
+    # A grid of time points (in days)
+    t = np.linspace(0, 183, 183)
+    # The SIR model differential equations.
+
+
+    def deriv(y, t, N, beta, gamma):
+        S, I, R = y
+        dSdt = -beta * S * I / N
+        dIdt = beta * S * I / N - gamma * I
+        dRdt = gamma * I
+        return dSdt, dIdt, dRdt
+
+
+    # Initial conditions vector
+    y0 = S0, I0, R0
+    # Integrate the SIR equations over the time grid, t.
+    ret = odeint(deriv, y0, t, args=(N, beta, gamma))
+    S, I, R = ret.T
+
+    # Plot the data on three separate curves for S(t), I(t) and R(t)
+    fig = plt.figure(facecolor='w')
+    ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+    ax.plot(t, S/5000000, 'b', alpha=0.5, lw=2, label='Susceptible')
+    print(S, "=S")
+    ax.plot(t, I/5000000, 'r', alpha=0.5, lw=2, label='Infected')
+    print(I, "=I")
+    ax.plot(t, R/5000000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+    print(R, "=R")
+    ax.set_xlabel('Time /days')
+    ax.set_ylabel('Number (5000000s)')
+    ax.set_ylim(0,1.2)
+    ax.yaxis.set_tick_params(length=0)
+    ax.xaxis.set_tick_params(length=0)
+    ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+    legend = ax.legend()
+    legend.get_frame().set_alpha(0.5)
+    for spine in ('top', 'right', 'bottom', 'left'):
+        ax.spines[spine].set_visible(False)
+    plt.show()
+
+
